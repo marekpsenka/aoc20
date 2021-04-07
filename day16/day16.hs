5d65844f24733ba114c50303294c6adb57d7cfac
Marek Pšenka <marek.psenka@thermofisher.com>
2021-04-07 19:51:33 +0200

    First star of d16 done

--- day16/day16.hs
+++ day16/day16.hs
@@ -8,6 +8,8 @@ data Rule = Rule { field :: String
                  , umax :: Int
                  } deriving (Show)
 
+type Ticket = [Int]
+
 getLinesUntilBlank :: [IO String] -> IO [String]
 getLinesUntilBlank = getLinesUntilBlank' []
   where
@@ -29,9 +31,28 @@ readRule s = let
           , umin=read sumin :: Int
           , umax=read sumax :: Int
           }
+
+readTicket :: String -> Ticket
+readTicket = map (read :: String -> Int) . splitOn ","
+
+isValidAgainst :: Int -> Rule -> Bool
+isValidAgainst i r = (lmin r <= i && i <= lmax r) ||
+               (umin r <= i && i <= umax r)
+
+invalidFieldsSum :: Ticket -> [Rule] -> Int
+invalidFieldsSum t rs = sum invalid
+  where
+    invalid = filter (\i -> not (any (isValidAgainst i) rs)) t
   
 main :: IO ()
 main = do
   ss <- getLinesUntilBlank $ repeat getLine
-  print (readRule (head ss))
-  print ss
+  let rules = map readRule ss
+
+  mt:_ <- getLinesUntilBlank $ repeat getLine
+  let myticket = readTicket mt
+
+  _:ts <- lines <$> getContents 
+  let tickets = map readTicket ts
+
+  print (sum $ map (\t -> invalidFieldsSum t rules) tickets)
