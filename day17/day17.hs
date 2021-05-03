5eacdaf1469b548fae03fa1f25e365ab938255b8
Marek Pšenka <marek.psenka@thermofisher.com>
2021-05-03 18:05:05 +0200

    Halfway through D17

--- day17/day17.hs
+++ day17/day17.hs
@@ -6,7 +6,9 @@ v3Add :: V3 -> V3 -> V3
 v3Add (x1, y1, z1) (x2, y2, z2) = (x1 + x2, y1 + y2, z1 + z2)
 
 neighborhood :: V3 -> Set.Set V3
-neighborhood v = Set.fromList $ [v3Add v (i, j, k) | i <- [-1..1], j <- [-1..1], k <- [-1..1]]
+neighborhood v = Set.delete v full
+  where
+    full = Set.fromList $ [v3Add v (i, j, k) | i <- [-1..1], j <- [-1..1], k <- [-1..1]]
 
 envelope :: Set.Set V3 -> Set.Set V3
 envelope s = Set.unions $ Set.map neighborhood s
@@ -17,6 +19,19 @@ rule act actNeighbors
   | actNeighbors == 3 = True
   | otherwise = False
 
+numActiveNeighbors :: V3 -> Set.Set V3 -> Int
+numActiveNeighbors v active = Set.size (Set.intersection (neighborhood v) active)
+
+transition :: Set.Set V3 -> Set.Set V3
+transition active = Set.union (Set.difference active (Set.fromList toDie)) (Set.fromList toLive)
+  where
+    toConsider = Set.toList (Set.union (envelope active) active)
+    trComponent v = rule (Set.member v active) (numActiveNeighbors v active)
+    trMap = zip toConsider (map trComponent toConsider)
+    toLive = map fst (filter snd trMap)
+    toDie = map fst (filter (not . snd) trMap)
+
+
 main :: IO ()
 main = do
   inpt <- lines <$> getContents
@@ -24,7 +39,7 @@ main = do
   let indices = [(i, j) | i <- [0..length inpt - 1], j <- [0..length (head inpt) - 1]]
       b = zip indices (concat inpt)
       a = Set.fromList $ map ((\(x, y) -> (x, y, 0)) . fst) (filter (('#'==). snd) b)
+      final = iterate transition a !! 6
 
-  print a
-  print (Set.size $ neighborhood (0, 0, 0))
+  print (Set.size final)
 
