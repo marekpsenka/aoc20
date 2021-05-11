534b4fa6c32469c04153446bf9e6730656989db2
Marek Pšenka <marek.psenka@thermofisher.com>
2021-05-11 11:08:51 +0200

    Day 19 done

--- day19/day19.hs
+++ day19/day19.hs
@@ -5,6 +5,8 @@ import Control.Monad
 import Data.Either
 import Data.List
 
+import qualified Data.Map as Map
+
 data Rule = Single Char | Seq [Int] | Alt Rule Rule deriving(Show)
 
 natural :: CharParser st Int
@@ -34,11 +36,11 @@ rule = do i <- anID
           return (i, r)
 
 
-buildParser :: [Rule] -> Rule -> CharParser st String
+buildParser :: Map.Map Int Rule -> Rule -> CharParser st String
 buildParser rs r =
   case r of
     Single c -> count 1 (char c)
-    Seq rrs -> concat <$> mapM (buildParser rs . (rs !!)) rrs
+    Seq rrs -> concat <$> mapM (buildParser rs . (rs Map.!)) rrs
     Alt r1 r2 -> try (buildParser rs r1) <|> buildParser rs r2
 
 main :: IO ()
@@ -48,8 +50,15 @@ main = do
     (rules, _messages) = break (all isSpace) inpt
     messages = tail _messages
     parsedRules = fromRight [] (mapM (parse rule "") rules)
-    sortedRules = map snd (sortOn fst parsedRules)
-    p = buildParser sortedRules (head sortedRules) <* eof
+    ruleMap = foldl (\m (k, v) -> Map.insert k v m) Map.empty parsedRules
+    p = buildParser ruleMap (ruleMap Map.! 0) <* eof
+    r42 = buildParser ruleMap (ruleMap Map.! 42)
+    r31 = buildParser ruleMap (ruleMap Map.! 31)
+    p2 = do ss1 <- many1 (try r42)
+            ss2 <- many1 r31 <* eof
+            guard $ length ss1 > length ss2
+            return (concat ss1 ++ concat ss2)
 
   print (length $ filter isRight (map (parse p "") messages))
+  print (length $ filter isRight (map (parse p2 "") messages))
 
